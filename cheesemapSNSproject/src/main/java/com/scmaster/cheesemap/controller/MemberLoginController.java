60f92f1bfb35195e964b7ed53e6e865a9dad3428
Administer <Administer@J>
2017-03-17 13:52:01 +0900

    2017-03-17 controller 2ea create woong2

---
+++ cheesemapSNSproject/src/main/java/com/scmaster/cheesemap/controller/MemberLoginController.java
@@ -0,0 +1,13 @@
+package com.scmaster.cheesemap.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberLoginController {
+	private static final Logger logger = LoggerFactory.getLogger(MemberLoginController.class);
+	
+	
+	
+}
