aca49c86396f1fd0ff1c7e74808788faeb49a551
Administer <Administer@J>
2017-04-06 16:03:23 +0900

    2017-04-06
    망해쓰요
    
    woong

--- cheesemapSNSproject/src/main/webapp/resources/js/logout.js
+++ cheesemapSNSproject/src/main/webapp/resources/js/logout.js
@@ -1,26 +1,19 @@
 $(function() {
-	
-	$("#logout").on('click',function(){		
-		bootbox.confirm({ 
-			  size: "small",
-			  message: "Are you sure?", 
-			  callback: function(result){				  
-			  if(result){				  
-				  
-				  bootbox.hideAll();	
-							  
-				  location.href="logout";
-				  
-			  }else{
-				  
-				  
-				  
-			  }
-			  
-			  /* result is a boolean; true = OK, false = Cancel*/ }
-			})
-		
+	$("#logout").on('click', function() {
+		bootbox.confirm({
+			size : "small",
+			message : "Are you sure?",
+			callback : function(result) {
+				if (result) {
+
+					bootbox.hideAll();
+
+					location.href = "logout";
+
+				} else {
+				}
+				/* result is a boolean; true = OK, false = Cancel */
+			}
+		})
 	});
-	
-	
 });
