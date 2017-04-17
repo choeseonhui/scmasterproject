0f82fd018f846c48fa236230344fd0c8fc275c9f
Administer <Administer@J>
2017-04-17 17:38:51 +0900

    wppaspfkasd

--- cheesemapSNSproject/src/main/webapp/resources/js/makebook.js
+++ cheesemapSNSproject/src/main/webapp/resources/js/makebook.js
@@ -1,37 +1,27 @@
 $(function() {
-	var select_img = {};
-	
 	$("#makebook-button").on("click", function() {
 		location.href="makebook";
 	});
-	
-	$("#select-img").on("click", function() {
-		console.log("신비로운너의모습");
-		select_img.push($(this).attr("img_no"));
-		console.log(select_img);
-	});
-	
 	getMyBoard();
 });
 
 function getMyBoard() {
+	var select_img = new Array();
 	var myBoardListDiv = document.getElementById("myBoardList");
 	var myBoardList = $("#myBoard").val();
 	if(myBoardList != undefined) {
 		var board = JSON.parse(myBoardList);
 		var myBoard = '';
 		myBoard += '<div id="select-img" class="row">';
-		/*myBoard += '<select multiple="multiple" class="image-picker show-html">';*/
 		$.each(board, function(index, item) {
 			myBoard += '<div class="col-sm-4 portfolio-item">';
 			myBoard += '<a href="#" class="portfolio-link" data-toggle="modal">';
-			myBoard += '<div class="j-caption caption">';
+			myBoard += "<div class='j-caption caption' img_no='"+item.boa_id+"'>";
 			myBoard += '<div class="caption-content">';
 			myBoard += '<i class="fa fa-check fa-3x"></i>';
 			myBoard += '</div>';
 			myBoard += '</div>';
-			/*myBoard += "<option data-img-src='"+ item.boa_photo_savefile +"' value='"+item.boa_id+"' class='j-img-responsive img-responsive'>";*/
-			myBoard += "<img src='"+ item.boa_photo_savefile +"' img_no='"+item.boa_id+"' class='j-img-responsive img-responsive'>";
+			myBoard += "<img src='"+ item.boa_photo_savefile +"' class='j-img-responsive img-responsive'>";
 			myBoard += '</a>'
 			myBoard += '</div>';
 		});
@@ -39,5 +29,22 @@ function getMyBoard() {
 		myBoard += '</div>';
 		myBoardListDiv.innerHTML = myBoard;
 	}
+
+	$(".j-caption").on("click", function() {
+		var select_item = $(this).attr("img_no");
+		$(this).css('opacity', '0');
+		$(this).toggleClass("selected");
+		toMakeBook(select_item);
+	});
+	
+	function toMakeBook(select_item) {
+		if(select_img.indexOf(select_item) < 0) {
+			select_img.push(select_item);
+			$(".selected").css('opacity', '0.5');
+		} else {
+			select_img.splice(select_img.indexOf(select_item), 1);
+		}
+		console.log(select_img);
+	}
 }
-/*myBoardList*/
+
