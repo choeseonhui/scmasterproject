2f8741e56693a1cef2e0e162c496be8749f5cce7
Administer <Administer@J>
2017-04-17 13:40:57 +0900

    asdfasdfasdfasdfas

--- cheesemapSNSproject/src/main/webapp/resources/js/makebook.js
+++ cheesemapSNSproject/src/main/webapp/resources/js/makebook.js
@@ -8,24 +8,29 @@ $(function() {
 function getMyBoard() {
 	var myBoardListDiv = document.getElementById("myBoardList");
 	var myBoardList = $("#myBoard").val();
-	var board = JSON.parse(myBoardList);
-	var myBoard = '';
-	console.log(board);
-	myBoard += '<div class="row">';
-	$.each(board, function(index, item) {
-		console.log(item);
-		myBoard += '<div class="col-sm-4 portfolio-item">';
-		myBoard += '<a href="#" class="portfolio-link" data-toggle="modal">';
-		myBoard += '<div class="j-caption caption">';
-		myBoard += '<div class="caption-content">';
-		myBoard += '<i class="fa fa-check fa-3x"></i>';
+	console.log(myBoardList);
+	if(myBoardList != undefined) {
+		var board = JSON.parse(myBoardList);
+		var myBoard = '';
+		console.log(board);
+		myBoard += '<div id="select" class="row">';
+		myBoard += '<select multiple="multiple" class="image-picker show-html">';
+		$.each(board, function(index, item) {
+			console.log(item);
+			myBoard += '<div class="col-sm-4 portfolio-item">';
+			myBoard += '<a href="#" class="portfolio-link" data-toggle="modal">';
+			myBoard += '<div class="j-caption caption">';
+			myBoard += '<div class="caption-content">';
+			myBoard += '<i class="fa fa-check fa-3x"></i>';
+			myBoard += '</div>';
+			myBoard += '</div>';
+			myBoard += "<option data-img-src='"+ item.boa_photo_savefile +"' class='j-img-responsive img-responsive'>";
+			myBoard += '</a>'
+				myBoard += '</div>';
+		});
+		myBoard += '</select>';
 		myBoard += '</div>';
-		myBoard += '</div>';
-		myBoard += "<img src='"+ item.boa_photo_savefile +"' class='j-img-responsive img-responsive'>";
-		myBoard += '</a>'
-		myBoard += '</div>';
-	});
-	myBoard += '</div>';
-	myBoardListDiv.innerHTML = myBoard;
+		myBoardListDiv.innerHTML = myBoard;
+	}
 }
 /*myBoardList*/
